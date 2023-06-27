36f12bb37631e2d9b92a9cb02a9e3df28fe4e0e6
KostiantynPopovych <kostiantyn.popovych.exp@gmail.com>
2023-06-27 14:46:11 -0700

    Add sleuth support (works with Slf4j log.info)

--- clients/src/main/java/com/distributed/clients/notification/NotificationRegisterRequest.java
+++ clients/src/main/java/com/distributed/clients/notification/NotificationRegisterRequest.java
@@ -1,7 +1,5 @@
 package com.distributed.clients.notification;
 
-import java.time.LocalDateTime;
-
 public record NotificationRegisterRequest(Integer toCustomerId, String toCustomerEmail, String sender, NotificationType type, String message) {
 }
 
